0c17cfee98344ad71e7d679418b67486ccda36c5
kaleb <82520535+kaleb-hub@users.noreply.github.com>
2021-05-05 06:05:01 +0300

    new commit added

--- src/welcome/hello.java
+++ src/welcome/hello.java
@@ -1,5 +1,40 @@
 package welcome;
 
-public class hello {
-System.out.println("hello world class working");
+public class hello implements register {
+	
+	@Override
+	public void insert() {
+		
+		
+	}
+
+	@Override
+	public void delete() {
+		
+		
+	}
+
+	@Override
+	public void edit() {
+		
+		
+	}
+
+	
+
+	@Override
+	public void search() {
+		
+		
+	}
+	
+	public static void main(String args[])
+	{
+System.out.println("---------------Welcome to Employee Recruiting System--------");
+System.out.println("---------------select yoour choice--------");
+System.out.println("---------------select 1 register--------");
+System.out.println("---------------select 2 search--------");
+System.out.println("---------------select 3 update--------");
+System.out.println("---------------select 4 delete--------");
+	}
 }
---
+++ src/welcome/register.java
@@ -0,0 +1,8 @@
+package welcome;
+
+public interface register {
+public void delete();
+public void edit();
+public void insert();
+public void search();
+}
