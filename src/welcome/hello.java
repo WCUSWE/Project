7b74106fb06985a4405638a9266e21699bd42807
User <User@10.198.75.40>
2021-05-05 12:48:23 +0300

    look

--- src/welcome/hello.java
+++ src/welcome/hello.java
@@ -3,31 +3,39 @@ package welcome;
 import java.util.Scanner;
 
 public class hello implements register {
-	
+	static Scanner s=new Scanner(System.in);
 	@Override
 	public void insert() {
-		
+		String name;
+		System.out.println("enter your name");
+		name=s.next();
 		
 	}
 
 	@Override
 	public void delete() {
-		
+		String name;
+		System.out.println("enter your name");
+		name=s.next();	
 		
 	}
 
 	@Override
 	public void edit() {
-		
+		String name;
+		System.out.println("enter your name");
+		name=s.next();
 		
 	}
 
 	
 
 	@Override
-	public void search(int id, String name) {
+	public void search(int id, String nam) {
 		
-		
+		String name;
+		System.out.println("enter your name");
+		name=s.next();
 	}
 	
 	public static void main(String args[])
@@ -41,7 +49,7 @@ System.out.println("---------------select 2 search--------");
 System.out.println("---------------select 3 update--------");
 System.out.println("---------------select 4 delete--------");
 System.out.println("enter your choice"); 
-Scanner s=new Scanner(System.in);
+
 choice=s.nextInt();
 switch(choice)
 {
