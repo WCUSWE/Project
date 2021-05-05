c9bbc029c708bb9bfb0b32b86e1759447ffa4c54
kaleb <82520535+kaleb-hub@users.noreply.github.com>
2021-05-05 15:08:45 +0300

    Update register.java

--- src/welcome/register.java
+++ src/welcome/register.java
@@ -6,26 +6,3 @@ public void edit();
 public void insert();
 public void search(int id, String depart);
 }
-package welcome;
-
-import java.util.Scanner;
-
-class Main {
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-
-    System.out.println("Enter name, age and salary:");
-
-    // String input
-    String name = myObj.nextLine();
-
-    // Numerical input
-    int age = myObj.nextInt();
-    double salary = myObj.nextDouble();
-
-    // Output input by user
-    System.out.println("Name: " + name); 
-    System.out.println("Age: " + age); 
-    System.out.println("Salary: " + salary); 
-  }
-}
